2b8f17bd3e9f46762aff4f3338cb9764ba4a6e3c
John Zhan <jzhan@adkgroup.com>
2024-01-23 13:24:31 -0500

    add the router

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,14 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
-
-import Posts from './components/posts';
-import Post from './components/post';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Posts from './components/posts'; // Assuming 'Posts' is a component
+import Post from './components/post'; // Assuming 'Post' is a component
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Posts />} />
-      <Route path="/posts/:id" element={<Post />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/posts/:id" element={<Post />} />
+      </Routes>
+    </Router>
   );
 }
 
